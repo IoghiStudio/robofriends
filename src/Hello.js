f87cb9d13066cc8c2c9115ff9133aa00d04545d0
Nicu <ioghigithublab@gmail.com>
2022-06-20 06:51:59 -0700

    added css file for hello component

--- src/Hello.js
+++ src/Hello.js
@@ -1,8 +1,14 @@
 import React from "react";
+import "./Hello.css";
 
 class Hello extends React.Component {
   render() {
-    return <h1>Hello Deni!</h1>
+    return(
+      <div>
+        <h1 className="title">I love pizza</h1>
+        <p>Who does not love pizza....</p>
+      </div>
+    )
   }
 }
 
