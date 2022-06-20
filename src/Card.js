c8b7bdda33863945ae05bbb5ec12c3481f0c4c7f
Nicu <ioghigithublab@gmail.com>
2022-06-20 08:38:05 -0700

    added random name props to robots.js , and used props in cards

--- src/Card.js
+++ src/Card.js
@@ -4,8 +4,8 @@ import "./card.css";
 class Card extends React.Component {
   render() {
     return(
-      <div className="bg-light-green dib br4 pa2 ma2 grow bw2 shadow-5">
-        <img alt="roboimage" src="https://robohash.org/buci"></img>
+      <div className="tc bg-light-green dib br4 pa2 ma2 grow bw2 shadow-5">
+        <img alt="roboimage" src={`https://robohash.org/${this.props.random}`}></img>
         <div>
           <h2>{this.props.name}</h2>
           <p>{this.props.email}</p>
