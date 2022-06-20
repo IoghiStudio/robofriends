bfb29bbef3890bbe1a097c3fd301ff45e226c658
Nicu <ioghigithublab@gmail.com>
2022-06-20 07:45:20 -0700

    deleted files that i didnt need

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
@@ -10,7 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Hello greeting={"te iubesc"}/>
     <App />
   </React.StrictMode>
 );
