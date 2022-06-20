17b781bfc899740306c1e65ee0e1409ed608b885
Nicu <ioghigithublab@gmail.com>
2022-06-20 09:44:48 -0700

    Added App component to hold all components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CardList from "./CardList";
+
+class App  extends React.Component {
+  render() {
+    return(
+      <CardList />
+    )
+  }
+}
+
+export default App;
--- src/CardList.js
+++ src/CardList.js
@@ -4,16 +4,17 @@ import { robots } from "./robots";
 
 class CardList extends React.Component {
   render() {
-    const cardArray = robots.map((user , i) => {
+    return(
+      <div>
+        {
+          robots.map((user , i) => {
       return <Card 
       key={robots[i].id} 
       name={robots[i].name} 
       email={robots[i].email} 
       random={robots[i].random} />
-    }) 
-    return(
-      <div>
-        {cardArray}
+        })
+        }
       </div>
     )
   }
