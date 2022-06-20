64834d10baee8a49e3856e1171f79d4ff202ce57
Nicu <ioghigithublab@gmail.com>
2022-06-20 08:23:01 -0700

    added robots array , created 10 diferent card using props

--- src/Card.js
+++ src/Card.js
@@ -4,11 +4,11 @@ import "./card.css";
 class Card extends React.Component {
   render() {
     return(
-      <div className="bg-light-green dib br4 pa2 ma2 grow">
+      <div className="bg-light-green dib br4 pa2 ma2 grow bw2 shadow-5">
         <img alt="roboimage" src="https://robohash.org/buci"></img>
         <div>
-          <h2>Nicu Iorga</h2>
-          <p>Ioghistudio20382gmail.com</p>
+          <h2>{this.props.name}</h2>
+          <p>{this.props.email}</p>
         </div>
       </div>
     )
