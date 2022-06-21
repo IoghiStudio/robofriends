e720aefd1a2db289c25108575e95c04b478b0a6f
Nicu <ioghigithublab@gmail.com>
2022-06-21 05:50:28 -0700

    added style to our Scroll Component

--- src/Scroll.js
+++ src/Scroll.js
@@ -3,7 +3,7 @@ import React from "react";
 class Scroll extends React.Component {
   render() {
     return(
-      <div>
+      <div style={{overflow: 'scroll', border: '5px solid black' , height: '500px'}}>
         {this.props.children}
       </div>
     )
