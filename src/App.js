ec024bafecd3bec0c726bc01df24d48f80319d67
Nicu <ioghigithublab@gmail.com>
2022-06-21 05:32:53 -0700

    added scroll component to wrap the searchbox

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import "./app.css";
+import Scroll from "./Scroll";
 
 class App  extends React.Component {
   constructor() {
@@ -34,7 +35,9 @@ class App  extends React.Component {
           <div className="tc">
             <h1>RoboFriends</h1>
             <SearchBox searchChange={this.onSearchChange} />
-            <CardList robots ={filteredRobots} />
+            <Scroll>
+              <CardList robots ={filteredRobots} />
+            </Scroll>
           </div>
         )
       }
