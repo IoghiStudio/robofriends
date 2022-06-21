ab3bd07c81e2e65ee75b1df0e0314caf2af3ef32
Nicu <ioghigithublab@gmail.com>
2022-06-21 05:07:52 -0700

    minor changes

--- src/CardList.js
+++ src/CardList.js
@@ -11,7 +11,7 @@ class CardList extends React.Component {
       key={this.props.robots[i].id} 
       name={this.props.robots[i].name} 
       email={this.props.robots[i].email} 
-      random={this.props.robots[i].random} />
+      random={this.props.robots[i].phone} />
         })
         }
       </div>
